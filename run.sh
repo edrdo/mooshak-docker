b8162d756ef7c3a7a59e64e98ff284e23e7b7c95
Eduardo Marques <edrdo@dcc.fc.up.pt>
2019-04-15 20:00:03 +0100

    Updated script [skip ci]

--- run.sh
+++ run.sh
@@ -7,5 +7,5 @@ PORT=$1
 DATA_DIR=$2
 
 echo == Starting Mooshak container -- host port: $PORT, host data dir: $DATA_DIR
-docker run --rm -h mooshak -v $DATA_DIR:/home/mooshak/data -p $PORT:80 -it --security-opt seccomp=unconfined mooshak-server/latest 
+docker run --rm -h mooshak -v $DATA_DIR:/home/mooshak/data -p $PORT:80 -it --security-opt seccomp=unconfined edrdo/mooshak
 exit $?
